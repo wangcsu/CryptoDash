c1a33ad2d51640ea08ebe1c75354e18f99a6d5c4
Ge Wang <wk.qwerty17@gmail.com>
2019-05-02 17:13:08 -0700

    add WelcomeMessage component

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Welcom(props) {
+  return <h1>Welcome to CryptoDash</h1>;
+}
---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import WelcomeMessage from './WelcomeMessage'
+
+class App extends Component {
+  render() {
+    return (
+      <WelcomeMessage />
+    )
+  }
+}
+
+export default App;
