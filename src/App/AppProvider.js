e00dc92e9b97154b7f64284011d958cfc80202c9
Ge Wang <wk.qwerty17@gmail.com>
2019-05-03 14:37:12 -0700

    Settings page and local storage

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export const AppContext = React.createContext();
 
@@ -6,18 +6,41 @@ export class AppProvider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 'settings',
-      setPage: this.setPage
-    }
+      page: "dashboard",
+      ...this.savedSettings(),
+      setPage: this.setPage,
+      confirmFavorites: this.confirmFavorites
+    };
   }
 
-  setPage = page => this.setState({ page })
+  confirmFavorites = () => {
+    this.setState({
+      firstVisit: false,
+      page: "dashboard"
+    });
+    localStorage.setItem(
+      "cryptoDash",
+      JSON.stringify({
+        test: "hello"
+      })
+    );
+  };
+
+  savedSettings() {
+    let cryptoDashData = JSON.parse(localStorage.getItem("cryptoDash"));
+    if (!cryptoDashData) {
+      return { page: "settings", firstVisit: true };
+    }
+    return {};
+  }
+
+  setPage = page => this.setState({ page });
 
   render() {
     return (
       <AppContext.Provider value={this.state}>
         {this.props.children}
       </AppContext.Provider>
-    )
+    );
   }
 }
--- src/App/WelcomeMessage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Welcom(props) {
-  return <h1>Welcome to CryptoDash</h1>;
-}
