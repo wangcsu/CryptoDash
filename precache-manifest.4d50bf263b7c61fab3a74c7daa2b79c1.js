967ed5aeb6daf00d2b1bc0ae808cac96a63f1346
wangcsu <wk.qwerty17@gmail.com>
2019-09-02 23:56:42 +0800

    Updates

---
+++ precache-manifest.4d50bf263b7c61fab3a74c7daa2b79c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aef44ce28bdb36909110b4a8e4d9606",
+    "url": "/CryptoDash/index.html"
+  },
+  {
+    "revision": "b5e14ebe4f52e80cabc7",
+    "url": "/CryptoDash/static/css/main.7fb5787a.chunk.css"
+  },
+  {
+    "revision": "a4a8209c12e3481aee03",
+    "url": "/CryptoDash/static/js/2.f05cdd38.chunk.js"
+  },
+  {
+    "revision": "b5e14ebe4f52e80cabc7",
+    "url": "/CryptoDash/static/js/main.6f5ac4de.chunk.js"
+  },
+  {
+    "revision": "5591c0be167dfc1d92f3",
+    "url": "/CryptoDash/static/js/runtime~main.dbe0dabc.js"
+  }
+]);
